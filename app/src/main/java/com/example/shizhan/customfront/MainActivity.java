85689eded08ffb26d61ad68e765ac1c3a76b4298
shizhan <ccp671108@163.com>
2016-07-24 12:00:00 +0800

    test

--- app/src/main/java/com/example/shizhan/customfront/MainActivity.java
+++ app/src/main/java/com/example/shizhan/customfront/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getSupportActionBar().hide();//隐藏标题栏
         setContentView(R.layout.activity_main);
     }
 }
